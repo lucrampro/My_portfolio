dff5884f273bf09125f336a6d47edb182c102270
Lucien Ramassamy <lucien.ramassamy@hetic.net>
2021-10-04 13:25:53 +0200

    Fix: changhe title

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'portfolio_2021',
+    title: 'Lucien - Développeur',
     htmlAttrs: {
       lang: 'en'
     },
